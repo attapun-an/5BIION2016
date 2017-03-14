1830a4e25e339f791aed19d85702217d9995f821
attapun-an <attapunanivat@gmail.com>
2017-03-14 22:11:09 +0700

    commit 2
    managed a working version with help from stack overflow.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-PlayerName = []
-file = open("NAMES.txt", "r")
-for i in range(1, 9):
-    PlayerName.append(file.readline())
-print(PlayerName)
+def reader():
+    player_name = []
+    file = open("NAMES.txt", "r")
+    for i in range(1, 9):
+        player_name.append(file.readline())
+    print(player_name)
+    file.close()
+
+
+def reader_v2():
+    player_name = []
+    file = open("NAMES.txt", "r").read().splitlines()
+    for line in file:
+        player_name.append(line)
+    print(player_name)
+
+reader()
+reader_v2()
